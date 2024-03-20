bafdd1d2dafe8224eeb0970de580f09686dd64d7
Shion1314 <jiaxinlin200204@gmail.com>
2024-03-20 12:05:55 -0400

    ss

--- bike-parking-app/app/lib/getCoordinates.ts
+++ bike-parking-app/app/lib/getCoordinates.ts
@@ -3,37 +3,15 @@ interface DataItem {
   latitude: string;
 }
 
-let cachedData: DataItem[] | null = null;
-
 async function getCoordinates(): Promise<DataItem[]> {
   try {
-    if (cachedData !== null) {
-      return cachedData;
-    }
+    const response = await fetch(`https://bike-parking.onrender.com/Parking_data/?X=-73.941009&Y=40.618406`);
+    const jsonData = await response.json();
 
-    let allData: DataItem[] = [];
-    let hasMoreData = true;
-    let page = 1;
-
-    while (hasMoreData) {
-      const response = await fetch(`https://bike-parking.onrender.com/Parking_data/?X=-73.941009&Y=40.618406&page=${page}`);
-      const jsonData = await response.json();
-
-      const data: DataItem[] = jsonData.map((item: any) => ({
-        longitude: item.x_coordinate.toString(),
-        latitude: item.y_coordinate.toString(),
-      }));
-
-      if (data.length === 0) {
-        hasMoreData = false;
-      } else {
-        allData = [...allData, ...data];
-        page++; // Increment page for next request
-      }
-    }
-
-    // Cache the fetched data
-    cachedData = allData;
+    const allData: DataItem[] = jsonData.map((item: any) => ({
+      longitude: item.x_coordinate.toString(),
+      latitude: item.y_coordinate.toString(),
+    }));
 
     return allData;
   } catch (error) {
