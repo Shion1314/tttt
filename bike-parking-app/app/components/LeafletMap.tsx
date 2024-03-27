a0e968a34e32846cded04b63cb8e9dd8ccc741c2
shion1314 <jiaxinlin200204@gmail.com>
2024-03-27 19:04:43 -0400

    u

--- bike-parking-app/app/components/LeafletMap.tsx
+++ bike-parking-app/app/components/LeafletMap.tsx
@@ -190,7 +190,7 @@ const MapComponent: FC = () => {
   }, []);
   const handlePost = async (marker: MarkerData) => {
     try {
-      const response = await fetch('https://bike-parking.onrender.com/favorite', {
+      const response = await fetch('https://localhost:3001/favorite', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
